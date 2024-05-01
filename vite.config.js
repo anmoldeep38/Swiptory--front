a98cac003dd2d9c0981dbfab8fdaf2f7bc5d1222
Anmol Deep <87927563+anmoldeep38@users.noreply.github.com>
2024-05-01 14:47:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://swiptory-i2xk.onrender.com',
         secure: false,
       },
     },
